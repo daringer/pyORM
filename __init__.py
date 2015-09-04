96610715a38bf39be703deb39b3253755d07f43c
Markus Meissner <coder@safemailbox.de>
2015-09-04 20:36:55 +0200

    add __init__.py in root for easy imports

---
+++ __init__.py
@@ -0,0 +1,11 @@
+
+from baserecord import BaseRecord
+
+#from baseview import BaseView
+#from fields import StringField, IntegerField, DateTimeField, \
+#        FloatField, OptionField, ManyToOneRelation, \
+#        ManyToManyRelation, OneToOneRelation
+
+import fields 
+
+from core import SQLiteDatabase, MemoryDatabase
